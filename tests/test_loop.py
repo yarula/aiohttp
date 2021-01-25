53c6f8340f39229389ac3a014b757bc07354e65e
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2021-01-25 13:07:10 +0100

    Auto-enable xdist when it's supported

--- tests/test_loop.py
+++ tests/test_loop.py
@@ -8,13 +8,14 @@ import pytest
 from aiohttp import web
 from aiohttp.helpers import PY_38
 from aiohttp.test_utils import AioHTTPTestCase, loop_context
+from tests._pytest_plugin import SUPPORTS_XDIST  # type: ignore[import]
 
 
 @pytest.mark.skipif(
     platform.system() == "Windows", reason="the test is not valid for Windows"
 )
 async def test_subprocess_co(loop: Any) -> None:
-    assert PY_38 or threading.current_thread() is threading.main_thread()
+    assert PY_38 or threading.current_thread() is threading.main_thread() or SUPPORTS_XDIST
     proc = await asyncio.create_subprocess_shell(
         "exit 0",
         stdin=asyncio.subprocess.DEVNULL,
